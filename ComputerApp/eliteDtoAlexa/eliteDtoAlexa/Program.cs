777466cb23e19c36d9a79c216d51f0a7c208cd0d
Austin <siraustin1234@gmail.com>
2016-11-16 17:40:25 -0500

    change

--- ComputerApp/eliteDtoAlexa/eliteDtoAlexa/Program.cs
+++ ComputerApp/eliteDtoAlexa/eliteDtoAlexa/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 using System.IO;
 using System.Linq;
 using System.Security.Permissions;
@@ -6,22 +7,24 @@ using System.Threading;
 
 using PubNubMessaging.Core;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
 
 public class Program
 {
     public class shipInfo
     {
         public string Event { get; set; }
-        public Rank rank;
-        public Progress progress;
-        public Docked docked;
-        public Location location;
-        public Touchdown touchdown;
-        public HullDamage hulldamage;
-        public ShieldState shieldstate;
-        public PVPKill pvpkill;
-        public DatalinkScan datalinkscan;
-        public RecieveText recievetext;
+        public Rank RankBuilder = new Rank();
+        public Progress ProgressBuilder = new Progress();
+        public Docked DockedBuilder = new Docked();
+        public Location LocationBuilder = new Location();
+        public Touchdown TouchdownBuilder = new Touchdown();
+        public HullDamage HullDamageBuilder = new HullDamage();
+        public ShieldState ShieldStateBuilder = new ShieldState();
+        public PVPKill PVPKillBuilder = new PVPKill();
+        public DatalinkScan DataLinkscanBuilder = new DatalinkScan();
+        public RecieveText RecieveTextBuilder = new RecieveText();
     }
     public class Rank
     {
@@ -31,6 +34,16 @@ public class Program
         public string Empire { get; set; }
         public string Federation { get; set; }
         public string CQC { get; set; }
+        public void rank(string combat, string trade, string explore, string empire, string federation, string cqc)
+        {
+            Combat = combat;
+            Trade = trade;
+            Explore = explore;
+            Empire = empire;
+            Federation = federation;
+            CQC  = cqc;
+        }
+        
     }
     public class Progress
     {
@@ -40,41 +53,86 @@ public class Program
         public string Empire { get; set; }
         public string Federation { get; set; }
         public string CQC { get; set; }
+        public void progress(string combat, string trade, string explore, string empire, string federation, string cqc)
+        {
+            
+            Combat = combat;
+            Trade = trade;
+            Explore = explore;
+            Empire = empire;
+            Federation = federation;
+            CQC = cqc;
+        }
     }
     public class Docked
     {
         public bool Dock { get; set; }
         public string StationName { get; set; }
         public string StationType { get; set; }
+        public void docked(bool dock, string stationname, string stationtype)
+        {
+            Dock = dock;
+            StationName = stationname;
+            StationType = stationname;
+        }
     }
     public class Location
     {
         public string StarSystem { get; set; }
         public string Body { get; set; }
+        public void location(string starsystem, string body)
+        {
+            StarSystem = starsystem;
+            Body = body;
+        }
     }
     public class Touchdown
     {
         public bool Landed { get; set; }
+        public void touchdown(bool landed)
+        {
+            Landed = landed;
+        }
     }
     public class HullDamage
     {
-        public decimal Damage { get; set; }
+        public double Damage { get; set; }
+        public void hulldamage(double damage)
+        {
+            Damage = damage;
+        }
     }
     public class ShieldState
     {
         public bool ShieldUp { get; set; }
+        public void shieldstate(bool shieldup)
+        {
+            ShieldUp = shieldup;
+        }
     }
     public class PVPKill
     {
         public string Victim { get; set; }
+        public void pvpkill(string victim)
+        {
+            Victim = victim;
+        }
     }
     public class DatalinkScan
     {
         public string Message { get; set; }
+        public void datalinkscan(string message)
+        {
+            Message = message;
+        }
     }
     public class RecieveText
     {
         public string Message { get; set; }
+        public void recievetext(string message)
+        {
+            Message = message;
+        }
     }
 
     private static AddedContentReader _continuousFileReader = null;
@@ -91,7 +149,23 @@ public class Program
     [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
     public static void Run()
     {
-        
+        shipInfo shipInfoMaster = new shipInfo();
+        shipInfoMaster.Event = "";
+        shipInfoMaster.RankBuilder.rank("", "", "", "", "", "");
+        shipInfoMaster.ProgressBuilder.progress("", "", "", "", "", "");
+        shipInfoMaster.DockedBuilder.docked(false, "", "");
+        shipInfoMaster.LocationBuilder.location("", "");
+        shipInfoMaster.TouchdownBuilder.touchdown(false);
+        shipInfoMaster.HullDamageBuilder.hulldamage(0.0);
+        shipInfoMaster.ShieldStateBuilder.shieldstate(true);
+        shipInfoMaster.PVPKillBuilder.pvpkill("");
+        shipInfoMaster.DataLinkscanBuilder.datalinkscan("");
+        shipInfoMaster.RecieveTextBuilder.recievetext("");
+
+        var serializer = new JavaScriptSerializer();
+        var json = serializer.Serialize(shipInfoMaster);
+
+        Console.WriteLine(json);
 
         System.Diagnostics.Process.GetCurrentProcess().PriorityClass = System.Diagnostics.ProcessPriorityClass.RealTime;
         string path = Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)).FullName;
